63232d8c19d6e96a9c45683b007bc12481031daf
Fahad Jafry <sfahadejaz@gmail.com>
2024-08-02 03:51:13 +0200

    SetVehicleParamsFromConfig

---
+++ app/custom/SetVehicleParamsFromConfig/VehicleConfig/src/main/java/VehicleConfig.java
@@ -0,0 +1,12 @@
+package custom.SetVehicleParamsFromConfig;
+
+public class VehicleConfig {
+    public int speedMode;
+    public double initialSetSpeed;
+
+    // // Constructor
+    // public VehicleConfig( int speedMode, double initialSetSpeed) {
+    //     this.speedMode = speedMode;
+    //     this.initialSetSpeed = initialSetSpeed;
+    // }
+}
