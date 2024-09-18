ac65dacc743fda01b92bd5abee7178d412cfe665
Fahad Jafry <sfahadejaz@gmail.com>
2024-09-18 21:43:05 +0200

    keep lane

---
+++ app/custom-applications/src/main/java/org/eclipse/mosaic/app/traci/ChangeLane.java
@@ -0,0 +1,112 @@
+package org.eclipse.mosaic.app.traci;
+
+import org.eclipse.mosaic.app.vehicleconfig.VehicleConfig;
+import org.eclipse.mosaic.fed.application.app.AbstractApplication;
+import org.eclipse.mosaic.fed.application.app.api.MosaicApplication;
+import org.eclipse.mosaic.fed.application.app.api.VehicleApplication;
+import org.eclipse.mosaic.fed.application.app.api.os.VehicleOperatingSystem;
+import org.eclipse.mosaic.interactions.application.ApplicationInteraction;
+import org.eclipse.mosaic.lib.objects.traffic.SumoTraciResult;
+import org.eclipse.mosaic.lib.objects.vehicle.VehicleData;
+import org.eclipse.mosaic.lib.util.scheduling.Event;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * This application slows down (or speeds up) the vehicle within the specified
+ * time in SUMO through
+ * the interface
+ * provided by MOSAIC which allows sending messages to TraCI and reacting on
+ * received TraCI response.
+ */
+public class ChangeLane extends AbstractApplication<VehicleOperatingSystem>
+        implements VehicleApplication, MosaicApplication {
+
+    private String lastSentMsgId;
+
+    /**
+     * Here a byte array is assembled that follows the traci protocol. With the byte
+     * array constructed
+     * here we instruct sumo directly to get the heading for the
+     * provided vehicle.
+     * <a href="https://sumo.dlr.de/docs/TraCI/Change_Vehicle_State.html">...</a>
+     *
+     * @param vehicleId Vehicle to set the speed mode of
+     * @return The byte array to be sent to sumo
+     */
+    private byte[] assembleTraciCommand(String vehicleId, SlowDownParams params) {
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        final DataOutputStream dos = new DataOutputStream(baos);
+        final byte TRACI_VEHICLE_CHANGE = (byte) 0xc4; // Bits Traci Command (0xc4 for vehicle value change)
+        final byte TRACI_SLOW_DOWN = (byte) 0x13; // Bits Traci Variable Identifier (0x14 for slow down)
+
+        try {
+            dos.writeByte(TRACI_VEHICLE_CHANGE);
+            dos.writeByte(TRACI_SLOW_DOWN);
+            dos.writeInt(vehicleId.length()); // Length of Vehicle Identifier
+            dos.write(vehicleId.getBytes(StandardCharsets.UTF_8)); // Vehicle Identifier
+            dos.writeByte(0x0f); // compound type
+            dos.writeInt(2); // number of parameters
+            dos.writeByte(0x8); // double type
+            dos.writeByte(1); // target speed
+            dos.writeByte(0xb); // double type
+            dos.writeDouble(25); // duration
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return baos.toByteArray();
+    }
+
+    @Override
+    public void onStartup() {
+        getLog().infoSimTime(this, "Startup");
+    }
+
+    @Override
+    public void onVehicleUpdated(@Nullable VehicleData previousVehicleData, @Nonnull VehicleData updatedVehicleData) {
+    }
+
+    @Override
+    public void onShutdown() {
+        getLog().infoSimTime(this, "Shutdown");
+    }
+
+    @Override
+    public void onSumoTraciResponded(SumoTraciResult sumoTraciResult) {
+        if (sumoTraciResult.getRequestCommandId().equals(lastSentMsgId)) {
+            getLog().infoSimTime(
+                    this,
+                    "Received TraCI message from Sumo. Vehicle will slow down");
+        }
+    }
+
+    @Override
+    public void onInteractionReceived(ApplicationInteraction applicationInteraction) {
+
+    }
+
+    @Override
+    public void processEvent(Event event) throws Exception {
+
+        Object resource = event.getResource();
+        if (resource instanceof VehicleConfig) {
+            // Initiate emergency brake
+            // getLog().infoSimTime(this, "Vehicle emergency break is requested through
+            // traci");
+            SlowDownParams params = new SlowDownParams(1, 25);
+
+            final byte[] traciMsg = assembleTraciCommand(getOs().getId(), params); // assemble the TraCI msg for sumo
+            String lastSentMsgId = getOs().sendSumoTraciRequest(traciMsg);
+            if (!lastSentMsgId.isEmpty()) {
+                getLog().infoSimTime(
+                        this,
+                        "Setting vehcile lane");
+            }
+        }
+    }
+}
